684ecec0e82ec2a29a5d54b9ae5043730bef9b97
Kyle Mwendwa <kklegendofgaming@gmail.com>
2025-03-16 11:38:44 +0300

    push

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(distance) {
   // Write your code here!
+  if (distance <= 400) {
+    return "This one is on me!";
+  } else if (distance > 400 && distance < 2000) {
+    return "That will be twenty bucks.";
+  } else if (distance > 2000 && distance < 2500) {
+    return "I will gladly take your thirty bucks.";
+  } else if (distance > 2500) {
+    return "No can do.";
+  }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city) {
   // Write your code here!
+  let possible = city === "NYC" ? "Ok, sounds good." : "No go.";
+  return possible;
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip) {
   // Write your code here!
+  let message;
+  switch (tip) {
+    case (tip = "generous"):
+      message = "Thank you so much.";
+      break;
+    case (tip = "not as generous"):
+      message = "Thank you.";
+      break;
+    case (tip = "thanks for everything"):
+      message = "Bye.";
+      break;
+  }
+  return message;
 }
